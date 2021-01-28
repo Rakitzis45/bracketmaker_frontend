b962672ceeb34d78afb41a0c1d739ad217847d5f
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-01-27 19:59:25 -0500

    added attributes to the bracket constructor

--- bracket.js
+++ bracket.js
@@ -1,8 +1,9 @@
 class Bracket {
-    constructor(name, totalTeams = 8, position1, position2, position3, position4, position5, 
+    constructor(id=nil, name, totalTeams = 8, position1, position2, position3, position4, position5, 
         position6, position7, position8, position9 = "EMPTY", 
         position10= "EMPTY", position11= "EMPTY", position12= "EMPTY", 
-        position13= "EMPTY", position14= "EMPTY", position15= "EMPTY"){
+        position13= "EMPTY", position14= "EMPTY", position15= "EMPTY", code= Math.floor(Math.random() * 10000) + 1){
+        this.id = id
         this.name = name;
         this.totalTeams = totalTeams;
         this.position1 = position1;
@@ -20,7 +21,8 @@ class Bracket {
         this.position13 = position13
         this.position14 = position14;
         this.position15 = position15;
-        this.code = Math.floor(Math.random() * 10000) + 1
+        this.code = code
+        this.user_id = user.id
     }
 
 
